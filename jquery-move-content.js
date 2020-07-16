fef960c3a67602b8f89d1dd697cfb568d7744b19
Priscila S Cunha <priscilacunha@msn.com>
2020-07-16 00:04:43 -0300

    Update jquery-move-content.js

--- jquery-move-content.js
+++ jquery-move-content.js
@@ -7,10 +7,10 @@ jQuery('.x').appendTo('.y');
 
 
 // Insert .x before .y
-jQuery('.x').insertBefore( jQuery('.y') );
+jQuery('.x').insertBefore('.y');
 
 // Insert .x after .y
-jQuery('.x').insertAfter( jQuery('.y') );
+jQuery('.x').insertAfter('.y');
 
 
 
