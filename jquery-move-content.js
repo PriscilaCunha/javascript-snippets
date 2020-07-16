c2a4a683a6787957c7b6544d163793d56a570062
Priscila S Cunha <priscilacunha@msn.com>
2020-07-15 23:00:42 -0300

    Create jquery-move-content.js

---
+++ jquery-move-content.js
@@ -0,0 +1,16 @@
+// Move .x to be inside .y at the beginning
+jQuery('.x').prependTo('.y');
+
+// Move .x to be inside .y at the end
+jQuery('.x').appendTo('.y'); 
+
+
+
+// Insert .x before .y
+jQuery('.x').insertBefore( jQuery('.y') );
+
+// Insert .x after .y
+jQuery('.x').insertAfter( jQuery('.y') );
+
+
+
