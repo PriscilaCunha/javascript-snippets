25c75ed6f0885385d13f1a69e31fd89f19105b97
Priscila S Cunha <priscilacunha@msn.com>
2020-07-16 00:10:34 -0300

    Update jquery-move-content.js

--- jquery-move-content.js
+++ jquery-move-content.js
@@ -1,16 +1,13 @@
-// Move .x to be inside .y at the beginning
+// Move .x to be inside .y at the beginning | Move .x para dentro de .y no início
 jQuery('.x').prependTo('.y');
 
-// Move .x to be inside .y at the end
+// Move .x to be inside .y at the end | Move .x para dentro de .y no fim
 jQuery('.x').appendTo('.y'); 
 
 
 
-// Insert .x before .y
+// Move .x before .y | Move .x para cima de .y
 jQuery('.x').insertBefore('.y');
 
-// Insert .x after .y
+// Move .x after .y | Move .x para baixo de .y
 jQuery('.x').insertAfter('.y');
-
-
-
